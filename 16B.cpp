cdabbccad2d58414a47c6d7ae6a2a71a13e559d5
Garv Nandwana <149328353+garvn10@users.noreply.github.com>
2024-10-08 22:56:04 +0530

    Add files via upload

---
+++ 16B.cpp
@@ -0,0 +1,21 @@
+//SHIVENDRA DWIVEDI
+//23070123122
+#include<iostream>
+using namespace std;
+
+int main(){
+    int age;
+    cout<<"Enter age: ";
+    cin>>age;
+    try{
+        if(age<18){
+            throw age;
+        }
+        else{
+            cout<<"Age: "<<age<<"\\nAPPROVED"<<endl;
+        }
+    }
+    catch(int a){
+        cout<<"\\nERROR: Underage! ("<<age<<")"<<endl;
+    }
+}
